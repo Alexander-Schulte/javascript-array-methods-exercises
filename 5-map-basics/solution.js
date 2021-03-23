2d11e2c59d89017a20d4fce12ab00f83cc6a2a1e
Alexander Schulte <alex11.schulte@googlemail.com>
2021-03-23 15:35:34 +0100

    Add all exercises

--- 5-map-basics/solution.js
+++ 5-map-basics/solution.js
@@ -1,3 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // Your code below
+const numbersMultiplyby2 = numbers.map((x) => x * 2);
+console.log(numbersMultiplyby2);
+
+const numbersDividedby2 = numbers.map((x) => x / 2);
+console.log(numbersDividedby2);
+
+const numbersMultiplybyThemselve = numbers.map((x) => x * x);
+console.log(numbersMultiplybyThemselve);
